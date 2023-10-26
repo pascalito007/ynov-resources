b6cf6260304f164c0fe0e0972e8a29ad090fc765
Jean-Pascal MEWENEMESSE <scalpacademy.io@gmail.com>
2023-10-26 04:02:30 +0200

    adding archi schema and set_env script file

---
+++ 2023/m2/docker/udagram/set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=<YOUR_POSTGRES_USER>
+export POSTGRES_PASSWORD=<YOUR_POSTGRES_PASSWORD>
+export POSTGRES_HOST=<YOUR_POSTGRES_HOST>
+export POSTGRES_DB=<YOUR_POSTGRES_DB_NAME>
+export AWS_BUCKET=udagram-bucket-1234567 # Please dont change this
+export AWS_REGION=us-east-1 #Please dont change this
+export AWS_PROFILE=default #Please dont change this
+export JWT_SECRET=testing #Please dont change this
+export URL=http://localhost:8100 #Please dont change this
